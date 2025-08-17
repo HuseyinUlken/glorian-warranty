cc1beb7d6515c791dca5919da145b18cdd2b2e32
Furkan Meclis <furkanmeclis@icloud.com>
2025-08-17 04:40:02 +0300

    build

---
+++ public/build/assets/skeleton-DWhk2XwR.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-DsTwdazc.js";import{c as r}from"./createLucideIcon-upp3zg0H.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
